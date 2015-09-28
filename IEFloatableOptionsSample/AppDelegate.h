7d905c7e1e206e9498fd043b17290da673e77630
Dinesh Kumar <dinesh_hudinwal@hotmail.com>
2015-09-28 17:37:00 +0530

    Initial Commit

---
+++ IEFloatableOptionsSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IEFloatableOptionsSample
+//
+//  Created by Dinesh Kumar on 28/09/15.
+//  Copyright © 2015 Dinesh Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IEFloatableOptionsSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IEFloatableOptionsSample
+//
+//  Created by Dinesh Kumar on 28/09/15.
+//  Copyright © 2015 Dinesh Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
